f1c8b29acedd538709d9ecc467caa62b59cd5d22
Manny Verma <mannyverma11@gmail.com>
2023-08-17 16:26:26 -0600

    fixing styling for page

--- ReactProjects/Ravenous/ravenous/src/App.js
+++ ReactProjects/Ravenous/ravenous/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './Business/business.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="Title">
           Ravenous
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
