77aa9a81cdbcdadeaee70b39f7f33a49c669f913
Manny Verma <mannyverma11@gmail.com>
2023-09-20 13:23:31 -0600

    adding initial project react

---
+++ ReactProjects/ReactRouter/starting_code/src/components/Author.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAuthors } from "../features/authors/authorsSlice";
+import { useParams, Link } from "react-router-dom";
+
+// Import useParams from React Router
+
+export default function Author () {
+
+  // Extract the `name` URL parameter.
+
+  const { name }= useParams();
+  const authors = useSelector(selectAuthors)
+  const author = authors[name]
+
+  return (
+    <main>
+      Articles by {name}
+      <ul>
+        {author && author.articles ? author.articles.map(slug => (
+          <li key={slug}>
+            <Link to={`/articles/${slug}`}>
+              {slug}
+            </Link>
+          </li>
+        )) : <p> No Articles Found...</p>}
+      </ul>
+    </main>
+  )
+}
